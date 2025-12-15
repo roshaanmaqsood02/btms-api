c7194165ae508a806d87b2e8e28368f0d5b630a1
Roshaan Maqsood <roshaan.maqsood@bracketsltd.email>
2025-12-15 19:13:37 +0500

    edit migration

--- src/migrations/1765545743757-Init.ts
+++ src/migrations/1765545743757-Init.ts
@@ -1,14 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Init1765545743757 implements MigrationInterface {
-    name = 'Init1765545743757'
+  name = 'Init1765545743757';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" ADD "ayyaz" character varying NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "users" ADD "ayyaz" character varying NOT NULL`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "ayyaz"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "roshaan"`);
+  }
 }
