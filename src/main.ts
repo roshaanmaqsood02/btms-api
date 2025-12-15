416762bdde19239d7b99c9c0289ad8fcd9a845b9
Roshaan Maqsood <roshaan.maqsood@bracketsltd.email>
2025-12-15 13:47:39 +0500

    add origin

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   }
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'http://localhost:3001'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
